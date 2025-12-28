86fd05954346d1ccd17b11d6e7fa4af01f8e8f6c
Subham-Shrestha <subham.shrestha008@gmail.com>
2025-12-28 10:09:14 +0545

    Update lenString.py

--- lenString.py
+++ lenString.py
@@ -1,2 +1,7 @@
 invisible = str(input("Enter a string : "))
 print(str(len(invisible)) + ' ' + 'is the length of string' + ' ' + str(invisible))
+
+
+# This is updated code.
+# Added comment for clarity
+# The code takes a string input from the user and prints its length along with the string itself
